ce3572f2b45ca90e34e96686e0b91021f8fdd3d7
tobywcj <tobywong42r@gmail.com>
2024-01-26 01:16:17 +0000

    debug

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # Import Clip, ClipSeg, and Stable Diffusion models
 import clip
-from clipseg.models.clipseg import CLIPDensePredT
+from clipseg import CLIPDensePredT
 from diffusers import StableDiffusionInpaintPipeline, EulerDiscreteScheduler
 
 
